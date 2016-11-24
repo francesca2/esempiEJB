95aefa34ba35836b07926fdf9ced669330918bf6
Rossella <corso@10.0.1.196>
2016-11-24 15:45:44 +0100

    Stateless

---
+++ EJBEsempioStateless/src/it/alfasoft/martina/Contatore.java
@@ -0,0 +1,38 @@
+package it.alfasoft.martina;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Contatore
+ */
+@Stateless
+
+public class Contatore implements IContatoreLocale{
+
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+    private int cnt;
+    
+    public Contatore(){
+    	System.out.println("Creato!");
+    }
+
+    public int getCnt() {
+	    return cnt;
+    }
+
+    public void setCnt(int cnt) {
+	    this.cnt = cnt;
+    }
+
+	@Override
+	public int incrementa() {
+		
+		return cnt++;
+	}
+   
+
+}
